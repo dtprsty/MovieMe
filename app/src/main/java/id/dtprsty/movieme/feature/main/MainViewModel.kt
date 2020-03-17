6878d8878435a833f1a7e8f98a5646f62919ac00
Dwi Teguh Prasetyo <dwi.teguh@meteor.id>
2020-03-17 16:45:50 +0700

    add libs

--- app/src/main/java/id/dtprsty/movieme/feature/main/MainViewModel.kt
+++ app/src/main/java/id/dtprsty/movieme/feature/main/MainViewModel.kt
@@ -1,5 +1,6 @@
 package id.dtprsty.movieme.feature.main
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
