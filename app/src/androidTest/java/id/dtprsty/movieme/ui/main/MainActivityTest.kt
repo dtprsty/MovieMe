a384e4075c52852772df3f715319aa22df7d9c56
Dwi Teguh Prasetyo <dwi.teguh@meteor.id>
2020-04-15 12:13:38 +0700

    change file unit test name

--- app/src/androidTest/java/id/dtprsty/movieme/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/id/dtprsty/movieme/ui/main/MainActivityTest.kt
@@ -18,7 +18,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class MovieActivityTest {
+class MainActivityTest {
     @Rule
     @JvmField
     var activityRule = ActivityTestRule(MainActivity::class.java)
